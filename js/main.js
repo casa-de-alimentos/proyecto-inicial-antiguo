ad5d69bb81842bc1008cfddd9ab958242d19b1b4
recker112 <joseortiz112001@gmail.com>
2020-04-13 09:01:22 +0000

    3 pantallas

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+$(document).ready(function(){
+    $('select').formSelect();
+});
+
 //Abrir drawer
 var drawerOpen = document.getElementById('openDrawer');
+var oscuroB = document.getElementById('oscuroB');
 
 drawerOpen.addEventListener('click', function(e) {
   var drawerBox = document.getElementById('drawer');
 
   $(drawerBox).animate({left: "0px"});
+	$(oscuroB).css({display: 'block'});
 });
 
 //Cerrar drawer
@@ -14,4 +20,16 @@ drawerClose.addEventListener('click', function(e) {
   var drawerBox = document.getElementById('drawer');
 
   $(drawerBox).animate({left: "-300px"});
+	$(oscuroB).css({display: 'none'});
+});
+
+//Open Submenu
+var openSub = document.querySelectorAll(".openSubmenu");
+
+openSub.forEach(function(item) {
+  item.addEventListener('click', function(){
+		var open = $(item).data('open');
+		
+		$('#'+open).slideToggle();
+	})
 });
