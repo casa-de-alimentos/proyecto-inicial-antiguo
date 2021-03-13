0d455884d759b20deb3ff06c67c1fa6db884bdd5
recker112 <joseortiz112001@gmail.com>
2021-03-13 00:25:53 +0000

    Sql init actualizado para bienes

--- init_db.sql
+++ init_db.sql
@@ -1,28 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb5ubuntu0.5
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 13-03-2021 a las 00:22:50
+-- Versión del servidor: 5.7.33-0ubuntu0.18.04.1
+-- Versión de PHP: 7.3.27-1+ubuntu18.04.1+deb.sury.org+1
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `casa_alimentacion`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `assistance_ben`
+--
 
 CREATE TABLE `assistance_ben` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `date` date NOT NULL,
   `peso` int(11) DEFAULT '0',
-  `beneficiary_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `beneficiary_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
-
+--
+-- Estructura de tabla para la tabla `assistance_emp`
+--
 
 CREATE TABLE `assistance_emp` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `employee_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `employee_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
-
+--
+-- Estructura de tabla para la tabla `beneficiarys`
+--
 
 CREATE TABLE `beneficiarys` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `cedula` bigint(11) NOT NULL,
   `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `apellido` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
@@ -30,71 +60,207 @@ CREATE TABLE `beneficiarys` (
   `nacimiento` date NOT NULL,
   `peso` int(11) DEFAULT NULL,
   `seguimiento` tinyint(1) DEFAULT '0',
-  `created_by` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `created_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `bienes`
+--
 
+CREATE TABLE `bienes` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `stock` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `employees`
+--
 
 CREATE TABLE `employees` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `cedula` bigint(11) NOT NULL,
   `nombre` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `apellido` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `sexo` set('M','F') COLLATE utf8_unicode_ci NOT NULL,
   `nacimiento` date NOT NULL,
   `telefono` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `created_by` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
+  `created_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
-
+--
+-- Estructura de tabla para la tabla `logs`
+--
 
 CREATE TABLE `logs` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `action` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
-
+--
+-- Estructura de tabla para la tabla `products`
+--
 
 CREATE TABLE `products` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `stock` int(11) NOT NULL,
-  `medida` set('Kg','Lts') COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`id`)
+  `medida` set('Kg','Lts') COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
-
+--
+-- Estructura de tabla para la tabla `storage`
+--
 
 CREATE TABLE `storage` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `action` set('added','removed') COLLATE utf8_unicode_ci NOT NULL,
   `stock` int(11) NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`id`)
+  `date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
 
-
+--
+-- Estructura de tabla para la tabla `users`
+--
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `username` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `password` longtext COLLATE utf8_unicode_ci NOT NULL,
-  `super_user` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `super_user` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-INSERT INTO users VALUES("1","admin","Administrador","$2y$10$yzDsu7v/DeZqDwaZB8tzeOtMpLUwySrEc5UVc1IHWxfJOPI7kTmFS","1");
+--
+-- Volcado de datos para la tabla `users`
+--
 
+INSERT INTO `users` (`id`, `username`, `name`, `password`, `super_user`) VALUES
+(1, 'admin', 'Administrador', '$2y$10$yzDsu7v/DeZqDwaZB8tzeOtMpLUwySrEc5UVc1IHWxfJOPI7kTmFS', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `assistance_ben`
+--
+ALTER TABLE `assistance_ben`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `assistance_emp`
+--
+ALTER TABLE `assistance_emp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `beneficiarys`
+--
+ALTER TABLE `beneficiarys`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `bienes`
+--
+ALTER TABLE `bienes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `storage`
+--
+ALTER TABLE `storage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `assistance_ben`
+--
+ALTER TABLE `assistance_ben`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `assistance_emp`
+--
+ALTER TABLE `assistance_emp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `beneficiarys`
+--
+ALTER TABLE `beneficiarys`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `bienes`
+--
+ALTER TABLE `bienes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `storage`
+--
+ALTER TABLE `storage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
